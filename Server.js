ee953ecf05c613b4c23cbed0c05a3ad8ff183eaf
Leon <leon.ben2892@gmail.com>
2019-10-07 14:29:39 +0300

    expanded routes to seperate js files

--- Server.js
+++ Server.js
@@ -5,6 +5,11 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
 const db = knex({
 	client: 'pg',
 	connection: {
@@ -19,82 +24,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-});
+app.get('/', (req, res) => { res.send(database.users) });
 
-app.post('/signin', (req, res) => {
-	db.select('email', 'hash').from('login').where('email', '=', req.body.email)
-		.then(data => {
-			const isUserValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if(isUserValid){
-				return db.select('*').from('users').where('email','=', req.body.email)
-				.then(user => {
-					res.json(user[0])
-				})
-				.catch(err => res.status(400).json('Unable to get user!'))
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 
-			} else {
-				res.status(400).json('Wrong credentials!');
-			}
-		})
-		.catch(err => res.status(400).json('Wrong credentials!'))
-});
-
-app.post('/register', (req, res) => {
-	const { email, name, password} = req.body;
-	const hash = bcrypt.hashSync(password, 10);
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-			.returning('*')
-			.insert({
-				email: loginEmail[0],
-				name: name,
-				joined: new Date()
-			}).then(user => {
-				res.json(user[0]);
-			})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-		.catch(err => res.status(400).json('Unable to register!'));
-});
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
 // :id - this syntax allows to enter anything after profile/ 
 // in order to get the string after profile/ we write req.params
 //for example: profile/4563
 // req.params = 4563
-app.get('/profile/:id', (req, res) => {
-	const {id} = req.params;
-	db.select('*').from('users').where({
-		id: id
-	})
-	.then(user => {
-		if(user.length > 0){
-			res.json(user[0]);
-		} else {
-			res.status(400).json('User not found!')
-		}	
-	})
-	.catch(err => res.status(400).json('Error getting user!'))
-});
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
-app.put('/image', (req, res) => {
-	const {id} = req.body;
-	db('users').where('id', '=', id).increment('entries', 1).returning('entries')
-	.then(entries => {
-		res.json(entries[0])
-	})
-	.catch(err => res.status(400).json('Unable to get entries'))
-});
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
 app.listen(3000, () => {
 	console.log('App is running on port 3000');
